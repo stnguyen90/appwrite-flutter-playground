825a35f7975d7d3b73d4e4916113a3d51fad8f8c
Steven Nguyen <1477010+stnguyen90@users.noreply.github.com>
2026-03-18 11:09:09 -0700

    Use environment variable for app title

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:core';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -31,7 +32,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: String.fromEnvironment("APP_TITLE", defaultValue: 'Flutter Demo Home Page')),
     );
   }
 }
